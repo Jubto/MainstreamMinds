fc57b119703bc3ba37cc87faec45ceb2de34e336
Mitchell Henry <mitchell.henry334@gmail.com>
2022-07-03 12:59:14 +1000

    Created Tag + UserTagLink tables + models

---
+++ backend/app/models/tag.py
@@ -0,0 +1,19 @@
+from typing import List, Optional
+
+from sqlmodel import SQLModel, Field, Relationship
+
+
+class TagBase(SQLModel):
+    # probably want index on this so we can have clean urls with fast query, ie site.com/fields/biology instead of
+    # site.com/fields/25
+    name: str = Field(index=True)
+
+
+class Tag(TagBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)  # primary key as int for fast joins with user/story
+
+    user_links = List["User"] = Relationship(back_populates="tag_links", link_model="UserTagLink")
+
+
+class TagRead(TagBase):
+    pass  # user only provides tag name
