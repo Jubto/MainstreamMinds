cde974f080921510ea352e7517449d65979cf043
HannahCharak <z5165292@cse.unsw.edu.au>
2022-07-08 17:05:48 +0300

    fixed repositories institution file

--- backend/app/repositories/institution.py
+++ backend/app/repositories/institution.py
@@ -4,20 +4,25 @@ from fastapi import Depends
 from sqlmodel import select, Session
 
 from app.db import get_session
-# from app.models.user import User, UserUpdate, UserCreate
-from app.models.institution import Institution
 from app.repositories.base import BaseRepository
+from app.models.institution import Institution, InstitutionRead
+# TODO: verify what the ResearchStory object looks like
+from app.models.story import ResearchStory
 
 
 class InstitutionRepository(BaseRepository[Institution]):
-    model = Institution
-
     def __init__(self, session: Session):
-        super(InstitutionRepository, self).__init__(Institution, session)
+        self.session = session
 
-    # def get_by_email(self, email: str) -> User:
-    #     return self.session.exec(select(User).where(User.email == email)).first()
+    def get_institution(self, current_story_id: int) -> List[Institution]:
+        return self.session.exec(select(ResearchStory).where(ResearchStory.id == current_story_id)).one().institution_links
 
+    # need to assess what info we want to pass in for an institution
+    def add_institution(self, current_story_id: int, institution: str):
+        pass
+
+    def create_institution(self, tag: InstitutionRead):
+        pass
 
 def get_institution_repository(session: Session = Depends(get_session)) -> InstitutionRepository:
     return InstitutionRepository(session)
