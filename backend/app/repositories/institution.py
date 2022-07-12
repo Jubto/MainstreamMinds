81122a2403fd2da61c860a9791e418e8c09423e5
HannahCharak <z5165292@cse.unsw.edu.au>
2022-07-12 22:35:34 +0300

    updated instituion repos - all function stubs

--- backend/app/repositories/institution.py
+++ backend/app/repositories/institution.py
@@ -15,14 +15,25 @@ class InstitutionRepository(BaseRepository[Institution]):
         self.session = session
 
     def get_institution(self, current_story_id: int) -> List[Institution]:
-        return self.session.exec(select(ResearchStory).where(ResearchStory.id == current_story_id)).one().institution_links
+        pass
+        # TODO
+        # return self.session.exec(select(ResearchStory).where(ResearchStory.id == current_story_id)).one().institution_links
 
     # need to assess what info we want to pass in for an institution
     def add_institution(self, current_story_id: int, institution: str):
         pass
+        # TODO
+        # story = self.session.exec(select(ResearchStory).where(ResearchStory.id == current_story_id)).one()
+        # db_institution = self.session.exec(select(Institution).where(Institution.name == institution)).one()
+        # story.tag_links.append(db_institution)
+        # self.session.commit()
 
     def create_institution(self, tag: InstitutionRead):
         pass
+        # TODO
+        # db_institution = Institution.from_orm(institution)
+        # self.session.add(db_institution)
+        # self.session.commit()
 
 def get_institution_repository(session: Session = Depends(get_session)) -> InstitutionRepository:
     return InstitutionRepository(session)
