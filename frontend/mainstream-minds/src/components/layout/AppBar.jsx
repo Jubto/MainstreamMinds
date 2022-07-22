351ad26590b76954e9f59f3f8a0357c9b6a06876
Jamie Clarisse Domingo <jdomingo@Jamies-MacBook-Pro.local>
2022-07-22 14:35:32 +1000

    RS-77-us-901: editing appbar

--- frontend/mainstream-minds/src/components/layout/AppBar.jsx
+++ frontend/mainstream-minds/src/components/layout/AppBar.jsx
@@ -1,9 +1,27 @@
 import { AppBar as MuiAppBar } from "@mui/material"
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import LoginIcon from '@mui/icons-material/Login';
+
 
 const AppBar = () => {
   return (
-    <MuiAppBar position="static">Mainstream Minds</MuiAppBar>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+        <MuiAppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Mainstream Minds
+          </Typography>
+          <Button color="inherit">DISCOVER</Button>
+          <Button color="inherit">SEARCH</Button>
+          {/* <Button color="inherit">PROFILE</Button> */}
+          <LoginIcon/>
+        </Toolbar>
+        </MuiAppBar>
+    </Box>
+    )
 }
 
-export default AppBar
+export default AppBar;
