b5f92a98ff829b8dc39b727387f302c06fb1b9a0
Jamie Clarisse Domingo <jdomingo@Jamies-MacBook-Pro.local>
2022-07-22 15:23:07 +1000

    RS-77-us-901: editing appbar

--- frontend/mainstream-minds/src/components/layout/AppBar.jsx
+++ frontend/mainstream-minds/src/components/layout/AppBar.jsx
@@ -4,24 +4,57 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import LoginIcon from '@mui/icons-material/Login';
+import { Link, useLocation } from "react-router-dom";
+import IconButton from '@mui/material/IconButton';
+import useAuth from "../../hooks/useAuth";
+
 
 
 const AppBar = () => {
+  const location = useLocation();
+  const { auth, setAuth } = useAuth();
+
+  const guest = (
+    <IconButton
+      size="large"
+      edge="start"
+      color="inherit"
+      aria-label="menu"
+      sx={{ mr: 2 }}
+      >
+        <LoginIcon/>
+    </IconButton>
+  );
+
+  const loggedIn = (
+    <>
+      <Button color="inherit">PROFILE</Button>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        >
+          <LoginIcon/>
+      </IconButton>
+    </>
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }}>
         <MuiAppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }} component={Link} to={'/'} state={{ from: location }}>
             Mainstream Minds
           </Typography>
-          <Button color="inherit">DISCOVER</Button>
-          <Button color="inherit">SEARCH</Button>
-          {/* <Button color="inherit">PROFILE</Button> */}
-          <LoginIcon/>
+          <Button color="inherit" component={Link} to={'/'} state={{ from: location }}>DISCOVER</Button>
+          <Button color="inherit" component={Link} to={'/search'} state={{ from: location }}>SEARCH</Button>
+          {auth.accessToken ? loggedIn : guest}
         </Toolbar>
         </MuiAppBar>
     </Box>
-    )
+  );
 }
 
 export default AppBar;
