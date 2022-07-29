0d54ceb9787799b6984d786beefef69250833ade
Mitchell Henry <mitchell.henry334@gmail.com>
2022-07-29 14:28:56 +1000

    Relationship between researchers and institutions

--- backend/app/models/institution.py
+++ backend/app/models/institution.py
@@ -5,6 +5,10 @@ from sqlalchemy import Column, Enum
 from sqlmodel import Relationship, SQLModel, Field
 
 
+class InstitutionResearcherLink(SQLModel, table=True):
+    institution_id: Optional[int] = Field(default=None, primary_key=True, foreign_key="institution.id")
+    researcher_id: Optional[int] = Field(default=None, primary_key=True, foreign_key="researcher.id")
+
 class InstitutionStoryLink(SQLModel, table=True):
     institution_id: Optional[int] = Field(default=None, primary_key=True, foreign_key="institution.id")
     story_id: Optional[int] = Field(default=None, primary_key=True, foreign_key="researchstory.id")
@@ -25,7 +29,7 @@ class Institution(InstitutionBase, table=True):
 
     # Institution contact - User object?
 
-    # researchers: List["Researcher"] = Relationship(back_populates="institutions", link_model="InstitutionResearcherLink")
+    researchers: List["Researcher"] = Relationship(back_populates="institution")
     # stories: List["ResearchStory"] = Relationship(back_populates="institutions", link_model=InstitutionStoryLink)
 
 
@@ -43,7 +47,4 @@ class InstitutionUpdate(SQLModel):
 class InstitutionCreate(InstitutionBase):
     pass
 
-# TODO: include once researcher exists
-# class InstitutionResearcherLink(SQLModel, table=True):
-#     institution_id: Optional[int] = Field(default=None, primary_key=True, foreign_key="institution.id")
-#     researcher_id: Optional[int] = Field(default=None, primary_key=True, foreign_key="researcher.id")
+
--- backend/app/models/researcher.py
+++ backend/app/models/researcher.py
@@ -3,7 +3,8 @@ from typing import Optional
 from datetime import datetime
 
 from sqlalchemy import Column, Enum
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+from app.models.institution import Institution
 
 
 class Role(int, enum.Enum):
@@ -15,13 +16,15 @@ class Role(int, enum.Enum):
 class ResearcherBase(SQLModel):
     bio: str = Field()
     mobile: str = Field()
-    institution_id: Optional[int]
+    institution_id: Optional[int] = Field(foreign_key="institution.id")
 
 
 class Researcher(ResearcherBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     date_verified: datetime = Field(default_factory=datetime.utcnow, nullable=False)
     user_id: int = Field(index=True, nullable=False, foreign_key="user.id", sa_column_kwargs={'unique': True})
+
+    institution: Optional[Institution] = Relationship(back_populates="researchers")
 
 
 class ResearcherCreate(ResearcherBase):
