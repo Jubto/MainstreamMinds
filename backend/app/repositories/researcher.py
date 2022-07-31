bdea70427c7e2f5efe6e2b403658f18b6167f8ac
jubto <kentocroft@gmail.com>
2022-07-31 18:57:12 +1000

    researcher error handling

--- backend/app/repositories/researcher.py
+++ backend/app/repositories/researcher.py
@@ -2,7 +2,7 @@ from typing import List
 
 from fastapi import Depends
 from sqlmodel import select, Session
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import NoResultFound, IntegrityError
 
 from app.db import get_session
 from app.models.researcher import ResearcherCreate, Researcher, ResearcherUpdate
@@ -15,13 +15,16 @@ class ResearcherRepository:
         self.session = session
 
     def add_researcher(self, new_researcher: ResearcherCreate, current_user_id: int) -> int:
-        to_add = Researcher()
-        assign_members_from_dict(to_add, new_researcher.dict(exclude_unset=True))
-        to_add.user_id = current_user_id
-        db_researcher = Researcher.from_orm(to_add)
-        self.session.add(db_researcher)
-        self.session.commit()
-        return db_researcher.id
+        try:
+            to_add = Researcher()
+            assign_members_from_dict(to_add, new_researcher.dict(exclude_unset=True))
+            to_add.user_id = current_user_id
+            db_researcher = Researcher.from_orm(to_add)
+            self.session.add(db_researcher)
+            self.session.commit()
+            return db_researcher.id
+        except IntegrityError:
+            raise NonExistentEntry('institution_id', new_researcher.institution_id)
 
     def get_researcher_by_id(self, researcher_id: int) -> Researcher:
         try:
