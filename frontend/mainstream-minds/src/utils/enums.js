cfdba40f7d915544e40c6462eb5c131af141b9b9
Eric Holmstrom <eric.holmstrom.sydney@gmail.com>
2022-08-05 15:10:02 +1000

    Fixed backend dockerfile, added blank docker compose file and adjusted so backend url is pulled from env in frontend build process

--- frontend/mainstream-minds/src/utils/enums.js
+++ frontend/mainstream-minds/src/utils/enums.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://127.0.0.1:8000/api'; // fastApi
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : 'http://127.0.0.1:8000/api'; // Backend base endpoint
