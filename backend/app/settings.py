9e4397e8e30234493b1f755e40a88f8740b0d797
Eric Holmstrom <eric.holmstrom.sydney@gmail.com>
2022-08-08 09:00:53 +1000

    Added auth consts to settings

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -7,6 +7,9 @@ class Settings(BaseSettings):
     DB_CONN: str
     REDIS_CONN: str = "redis://localhost:6379"
     CACHE_SIZE: int = 1000
+    SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    ALGORITHM:str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env.local"
