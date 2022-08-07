79dc163906e77e46d8d853e1ef04df9d571185be
HannahCharak <z5165292@cse.unsw.edu.au>
2022-08-07 20:06:36 +1000

    successfully abe to fill the database with institutions

--- backend/app/routers/populate.py
+++ backend/app/routers/populate.py
@@ -1,14 +1,18 @@
+from app.models.user import UserCreate
+from app.routers import researcher
 from fastapi import APIRouter, Depends, Path
 import requests
 import json
 
+
 from app.core.security import get_request_user_id, is_consumer, is_researcher
 from app.models.institution import InstitutionRead, InstitutionCreate, InstitutionUpdate
 from app.models.pagination import Page, Paginator, get_paginator
+from app.models.researcher import Researcher, ResearcherCreate
+from app.models.research_story import ResearchStoryCreate
 from app.services.institution import InstitutionService
-from app.models.researcher import Researcher
-from app.models.research_story import ResearchStoryShortRead
-from app.services.institution import InstitutionService
+from app.services.researcher import ResearcherService
+from app.services.user import UserService
 
 
 router = APIRouter(tags=['populate'])
@@ -21,45 +25,68 @@ router = APIRouter(tags=['populate'])
 async def populate_universities(
         n: int,
         institution_service: InstitutionService = Depends(InstitutionService),
+        user_service: UserService = Depends(UserService),
+        researcher_service: ResearcherService = Depends(ResearcherService),
+        paginator: Paginator = Depends(get_paginator),
 ):
+    # populate_universities(n, institution_service)
+    populate_researchers(n, user_service, researcher_service, paginator)
+    # populate_service.populate_research_stories()
     
-    logo = 'https://www.universitiesaustralia.edu.au/wp-content/themes/universities-australia/img/ua-logo-white.png'
-    inst = InstitutionCreate(name='newUni', location='Sydney', year_established=2020, logo=logo) 
-    r = institution_service.create_institution(inst)
+    return f'added {n} new institutions'
 
 
-    return f'added {n} new stories: {r}'
-
-@router.post(
-    "/", 
-    description='populates database',
-    # dependencies=[Depends(is_researcher)] 
-)
-async def populate_researchers(
-        n: int,
+def populate_universities(
+        n: int, 
         institution_service: InstitutionService = Depends(InstitutionService),
-):
-    
-    logo = 'https://www.universitiesaustralia.edu.au/wp-content/themes/universities-australia/img/ua-logo-white.png'
-    inst = InstitutionCreate(name='newUni', location='Sydney', year_established=2020, logo=logo) 
-    r = institution_service.create_institution(inst)
+    ):
+        # script_dir = os.path.dirname(__file__)
+        # file_path = script_dir + 'institutions.json'
+        file_path = '/mnt/c/Users/HCharak/OneDrive/Documents/UNSW-DESKTOP-UK70UPK/COMP9323/research-stories/backend/app/data/institutions.json'
+        with open(file_path) as json_file:
+            uni_data = json.load(json_file)
+            for institution in uni_data:
+                inst = InstitutionCreate(name=institution['name'], 
+                                        location=institution['address'], 
+                                        year_established=institution['year_established'], 
+                                        logo=institution['logo']) 
+                institution_service.create_institution(inst)
+
+        return f'added {n} new institutions'
 
 
-    return f'added {n} new stories: {r}'
-
-@router.post(
-    "/", 
-    description='populates database',
-    # dependencies=[Depends(is_researcher)] 
-)
-async def populate_research_stories(
+def populate_researchers(
         n: int,
-        institution_service: InstitutionService = Depends(InstitutionService),
-):
-    
-    logo = 'https://www.universitiesaustralia.edu.au/wp-content/themes/universities-australia/img/ua-logo-white.png'
-    inst = InstitutionCreate(name='newUni', location='Sydney', year_established=2020, logo=logo) 
-    r = institution_service.create_institution(inst)
+        user_service: UserService = Depends(UserService),
+        researcher_service: ResearcherService = Depends(ResearcherService),
+        paginator: Paginator = Depends(get_paginator),
+    ):
+        
+        # file_path = '/mnt/c/Users/HCharak/OneDrive/Documents/UNSW-DESKTOP-UK70UPK/COMP9323/research-stories/backend/app/data/users.json'
+        # with open(file_path) as json_file:
+        #     user_data = json.load(json_file)
+        #     for user in user_data:
+        #         user_obj = UserCreate(first_name = user['first_name'], 
+        #                               last_name = user['last_name'], 
+        #                               email = user['email'], 
+        #                               password = user['password']) 
+        #         user_service.create_user(user_obj)
+
+        # file_path = '/mnt/c/Users/HCharak/OneDrive/Documents/UNSW-DESKTOP-UK70UPK/COMP9323/research-stories/backend/app/data/researchers.json'
+        
+        
+        # researcher_info = ResearcherCreate(bio = user['bio'],
+        #                                    institution = user['institution'])
+
+        # researcher_id = researcher_service.upgrade(researcher_info, id)
+        print(get_users(paginator, user_service))
 
 
-    return f'added {n} new stories: {r}'
+        return f'added {n} new researchers'
+
+
+def get_users(
+    paginator: Paginator = Depends(get_paginator),
+    user_service: UserService = Depends(UserService)
+    ):
+    return user_service.get_all(paginator)
