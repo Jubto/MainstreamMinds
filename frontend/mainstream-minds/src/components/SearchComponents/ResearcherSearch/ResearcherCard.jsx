793613be4c99a0127fccff5e4cc10beddd6eec76
Jamie Clarisse Domingo <jdomingo@Jamies-MacBook-Pro.local>
2022-08-08 17:13:50 +1000

    added tags

--- frontend/mainstream-minds/src/components/SearchComponents/ResearcherSearch/ResearcherCard.jsx
+++ frontend/mainstream-minds/src/components/SearchComponents/ResearcherSearch/ResearcherCard.jsx
@@ -1,29 +1,31 @@
 import { Avatar, Typography } from "@mui/material"
 import { useState } from "react"
-import { ResearcherCardContainer, ResearcherDetails, ResearcherInstitution, ResearcherName } from "./ResearcherStyles"
+import { ResearcherCardContainer, ResearcherCardContents, ResearcherDetails, ResearcherInstitution, ResearcherName, ResearcherTags } from "./ResearcherStyles"
 import { getColourForString } from "../../../components/styles/colours"
 import { useEffect } from "react"
 import msmAPI from "../../../api/msmAPI"
+import Tags from "../../layout/Tags"
 
 const ResearcherCard = (props) => {
   const user = props.value.user
   const institution_id = props.value.institution_id
+  const institutionPosition = props.value.institution_position
+  const preferences = user.preference_tags
   const [institutionName, setIntitutionName] = useState('')
-  const [institutionPosition, setInstitutionPosition] = useState('')
+
+  const avatarSize = '70px'
 
   
   const bgColour = getColourForString(user.first_name+user.last_name)
-  const firstName = user.first_name
   console.log(props.value)
   console.log(user.first_name, user.last_name)
-  console.log(institutionName)
+  console.log(institutionName, institutionPosition)
 
   useEffect(() => {
     if(institution_id) {
       msmAPI.get(`/institutions/${institution_id}`)
         .then((res) => {
           setIntitutionName(res.data.name)
-          setInstitutionPosition(res.data.institution_postition)
         })
         .catch((err) => console.error(err))
     }
@@ -31,19 +33,24 @@ const ResearcherCard = (props) => {
 
   return(
     <ResearcherCardContainer>
-      {/* <p>{props.value}</p> */}
-      <Avatar sx={{ bgcolor: bgColour, width: 58, height: 58, mr: 1, marginRight: '16px' }} />
-      <ResearcherDetails>
-        <ResearcherName>
-          {user.first_name} {user.last_name}
-        </ResearcherName>
-        {institutionName && <Typography variant="body2" color="text.secondary">
-          {institutionName}
-        </Typography>}
-        {institutionPosition && <Typography variant="body2">
-          {institutionPosition}
-        </Typography>}
-      </ResearcherDetails>
+      <ResearcherCardContents>
+        {/* <p>{props.value}</p> */}
+        <Avatar sx={{ bgcolor: bgColour, width: avatarSize, height: avatarSize, mr: 1, marginRight: '16px' }} />
+        <ResearcherDetails>
+          <ResearcherName>
+            {user.first_name} {user.last_name}
+          </ResearcherName>
+          <Typography variant="body2" color="text.secondary" sx={{margin: '4px 0'}}>
+            {institutionName || "_"}
+          </Typography>
+          <Typography variant="body2">
+            {institutionPosition || "_"}
+          </Typography>
+        </ResearcherDetails>
+      </ResearcherCardContents>
+      <ResearcherTags>
+        <Tags tagSize="small" tags={preferences} />
+      </ResearcherTags>
     </ResearcherCardContainer>
 
   )
